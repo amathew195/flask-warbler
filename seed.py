37f71afa6b56ca8d5a6b4bbd1c666f5a46dfe281
Ashley Mathew <amathew195@gmail.com>
2023-01-07 16:34:20 -0600

    updated seed

--- seed.py
+++ seed.py
@@ -3,9 +3,12 @@
 from csv import DictReader
 from app import db
 from models import User, Message, Follows, Likes
+from dotenv import load_dotenv
 
 db.drop_all()
 db.create_all()
+
+load_dotenv()
 
 with open('generator/users.csv') as users:
     db.session.bulk_insert_mappings(User, DictReader(users))
